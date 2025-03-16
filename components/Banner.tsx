05de0ea8fbb524768d18e0570efa7afa0bc1007c
mohamedimem <imem.hamdi@etudiant-isi.utm.tn>
2025-03-16 16:49:13 +0100

    fix image to deploy

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,4 +1,6 @@
-interface BannerPorps {
+import Image from "next/image"; // Import Next.js Image component
+
+interface BannerProps {
   images: {
     src: string;
     name: string;
@@ -6,26 +8,32 @@ interface BannerPorps {
   speed: number;
 }
 
-export function ImageSection({ images, speed }: BannerPorps) {
+export function ImageSection({ images, speed }: BannerProps) {
   const imagesStyle = {
     animation: `swipe ${speed}ms linear infinite`,
   };
 
   return (
-    <div className='images' style={imagesStyle}>
-      {images.map(({ src, name }) => (
-        <div className='image'>
-          <img src={src} alt={name} />
+    <div className="images" style={imagesStyle}>
+      {images.map(({ src, name }, index) => (
+        <div key={index} className="image"> {/* Added 'key={index}' */}
+          <Image
+            src={src}
+            alt={name}
+            width={150}    // Set the width of the image
+            height={160}   // Set the height of the image
+            objectFit="cover"
+          />
         </div>
       ))}
     </div>
   );
 }
 
-export default function Banner({ images, speed }: BannerPorps) {
+export default function Banner({ images, speed }: BannerProps) {
   return (
-    <div className='banner-wrapper'>
-      <div className='wrapper'>
+    <div className="banner-wrapper">
+      <div className="wrapper">
         <ImageSection images={images} speed={speed} />
         <ImageSection images={images} speed={speed} />
       </div>
